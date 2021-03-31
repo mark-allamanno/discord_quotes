e69573e9e3b4342dff72b39d9c3307d464117dc1
Mark Allamanno <mallaman@ucsc.edu>
2021-03-31 01:26:36 -0700

    Added a simple feature to remind us to support one another

---
+++ feel_good.py
@@ -0,0 +1,34 @@
+from discord.ext import commands
+import discord
+
+import dotenv
+
+import os
+
+
+# Create a new bot with the prefix of '$' for commands
+BOT = commands.Bot(command_prefix='$')
+
+# Load the environment variables from the .env file and then get the discord api token
+dotenv.load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+
+@BOT.event
+async def on_ready() -> None:
+    """When the client connects to the discord server get the right channel and send the reminder before exiting"""
+
+    # Get the correct server for this command to be sent in and then get the correct channel in th server as well
+    server = discord.utils.find(lambda s: s.name == 'Test Server', BOT.guilds)
+    channel = discord.utils.find(lambda c: c.name == 'feel-good', server.channels)
+
+    # Then send in the chat telling people to engage in the activity
+    await channel.send(f'<@everyone> Its that time of day again, please post the best part about your day in the chat. '
+                       f'This is pseudo-mandatory meaning I cannot enforce it but you really should or else your peers '
+                       f'will be annoyed with you. Have a good night :)')
+
+    exit(0)  # Then we have done all we want to do, exit
+
+
+if __name__ == '__main__':
+    BOT.run(TOKEN)  # Application entry point
