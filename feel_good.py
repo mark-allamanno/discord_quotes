43ba010fa153df21ff4b646da7ea427fd7873ab1
Mark Allamanno <mallaman@ucsc.edu>
2021-04-08 00:46:06 -0700

    Added another daily message to remind members to do their breaks

--- feel_good.py
+++ feel_good.py
@@ -4,7 +4,7 @@ import discord
 import dotenv
 
 import os
-
+import sys
 
 # Create a new bot with the prefix of '$' for commands
 BOT = commands.Bot(command_prefix='$')
@@ -19,13 +19,20 @@ async def on_ready() -> None:
     """When the client connects to the discord server get the right channel and send the reminder before exiting"""
 
     # Get the correct server for this command to be sent in and then get the correct channel in th server as well
-    server = discord.utils.find(lambda s: s.name == 'Test Server', BOT.guilds)
+    server = discord.utils.find(lambda s: s.name == 'STEM Rehab', BOT.guilds)
     channel = discord.utils.find(lambda c: c.name == 'feel-good', server.channels)
 
+    if sys.argv[1] == 'Break Reminder':
+        message = "Its that time of day again, please begin your regularly scheduled break. Also post what you plan " \
+                  "to do with this break in chat so we can validate you are actually doing it. Also please do your " \
+                  "best to enjoy it :) "
+    else:
+        message = "Its that time of day again, please post the best part about your day in the chat. This is " \
+                  "pseudo-mandatory meaning I cannot enforce it but you really should or else your peers will be " \
+                  "annoyed with you. Have a good night :) "
+
     # Then send in the chat telling people to engage in the activity
-    await channel.send(f'@everyone Its that time of day again, please post the best part about your day in the chat. '
-                       f'This is pseudo-mandatory meaning I cannot enforce it but you really should or else your peers '
-                       f'will be annoyed with you. Have a good night :)')
+    await channel.send(f'@everyone {message}')
 
     await BOT.close()  # Then shut the bot down as that is all we need it to do, runs via crontab at 11pm daily
 
